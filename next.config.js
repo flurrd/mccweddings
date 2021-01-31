558580114d435ba248b2e5b57e841c128e51b376
marcushallyo <marcus@prosperdigital.com.au>
2021-01-31 12:34:31 +1100

    erterg

--- next.config.js
+++ next.config.js
@@ -17,29 +17,8 @@ module.exports = withSCSS({
   },
    exportPathMap: async function() {
     const routes = {
-      '/': { page : '/'},
-      "/404": { page: "/404"}
+      '/': { page : '/'}
     }
-    //get all .md files in the posts dir
-    const blogs = glob.sync('src/posts/**/*.md')
-    //get all .md files in the posts dir
-    const projects = glob.sync('src/projects/**/*.md')
-
-    //remove path and extension to leave filename only
-    const blogSlugs = blogs.map(file => file.split('/')[2].replace(/ /g, '-').slice(0, - 3).trim())
-    //remove path and extension to leave filename only
-    const projectSlugs = projects.map(file => file.split('/')[2].replace(/ /g, '-').slice(0, - 3).trim())
-
-    //add each blog to the routes obj
-    blogSlugs.forEach(blog => {
-      routes[`/blog/${blog}`] = { page: '/blog/[slug]', query: { slug: blog } };
-    });
-
-    //add each blog to the routes obj
-    projectSlugs.forEach(project => {
-      routes[`/project/${project}`] = { page: '/project/[slug]', query: { slug: project } };
-    });
-
     return routes
   }
 })
