8b6b22c3ee3b09355b4a84f747fe9fac5d648dfc
marcushallyo <marcus@prosperdigital.com.au>
2021-02-01 19:34:27 +1100

    sdfsfd

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -3,8 +3,8 @@ const Meta = (props) => (
 <Head>
 <title>Celebrant | Maureen Catherine Crawley</title>
 <meta name="description" content={props.desc} />
-<link rel="icon" type="image/png" href="/static/images/favicon.ico" />
-<link rel="apple-touch-icon" href="/static/images/favicon.ico" />
+<link rel="icon" type="image/png" href="../static/favicon.ico" />
+<link rel="apple-touch-icon" href="../static/favicon.ico" />
 <script src="https://kit.fontawesome.com/595162865f.js" crossOrigin="anonymous"></script>
 </Head>
 )
